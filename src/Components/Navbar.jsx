8a59992e23fdbd52bc1576ce16ca3f281fe91a3e
Lusapho Matiti <lusaphomatiti07@gmail.com>
2024-07-28 16:53:29 +0200

    Updated css

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styling/Navbar.css";
+import { socialLinks } from "../data";
+import { homePageLinks } from "../data";
+
+const Navbar = () => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src="LM.jpg" alt="Lusapho Matiti" />
+            </Link>
+
+            <button
+              type="button"
+              className="nav-toggle"
+              id="nav-toggle"
+              onClick={toggleMenu}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+
+          <ul className="nav-links" id="nav-links">
+            {homePageLinks.map((link) => {
+              const { id, href, text } = link;
+
+              <li key={id}>
+                <Link to={href} className="nav-link">
+                  {text}
+                </Link>
+              </li>;
+            })}
+          </ul>
+
+          <ul className="nav-icons">
+            {socialLinks.map((link) => {
+              const { id, href, icon } = link;
+
+              if (icon === "fas fa-envelope") {
+                return (
+                  <li key={id}>
+                    <a
+                      href={`mailto:clint@cnccustoms.co.za`}
+                      className="nav-icon"
+                      rel="noopener noreferrer"
+                    >
+                      <i className={icon}></i>
+                    </a>
+                  </li>
+                );
+              }
+
+              return (
+                <li key={id}>
+                  <a
+                    href={href}
+                    target="_blank"
+                    className="nav-icon"
+                    rel="noopener noreferrer"
+                  >
+                    <i className={icon}></i>
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+
+      <div className={`side-menu ${showMenu ? "show" : ""}`}>
+        <button className="close-btn" onClick={toggleMenu}>
+          <i className="fas fa-times"></i>
+        </button>
+
+        {homePageLinks.map((link) => {
+          const { id, href, text } = link;
+
+          <a key={id} href={href}>
+            {text}
+          </a>;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+
+export const homePageLinks = [
+  { id: 1, href: "#about", text: "About" },
+  { id: 2, href: "#contact", text: "Contact" },
+  { id: 3, href: "#CV", text: "CV" },
+  { id: 4, href: "#projects", text: "Project" },
+];
+
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://www.linkedin.com",
+    icon: "fa-brands fa-linkedin",
+  },
+  {
+    id: 2,
+    href: "https://www.instagram.com",
+    icon: "fab fa-instagram",
+  },
+  { id: 3, href: "https://www.gmail.com", icon: "fas fa-envelope" },
+];
