328195b4cdcf75dd1ffffe8aafcd9c320f9e13fd
Lusapho Matiti <lusaphomatiti07@gmail.com>
2025-03-17 20:39:27 +0200

    Fix unused variable and improve alt attribute

--- src/pages/LandPage.jsx
+++ src/pages/LandPage.jsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "../styling/LandPage.css";
 import "../index.css";
 
 const LandPage = () => {
-  const [scrollY, setScrollY] = useState(0);
-
   useEffect(() => {
     const handleScroll = () => {
       setScrollY(window.scrollY);
